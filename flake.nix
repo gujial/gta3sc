17876c64338e592ceee1fe5dbce2f77bfbe50e5b
gujial <gu18647403665@outlook.com>
2026-02-04 11:25:35 +0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "GTA3 Script Compiler/Decompiler";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "gta3sc";
+          version = "1.0";
+          src = self;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            gcc
+          ];
+
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Release"
+          ];
+
+          postInstall = ''
+            mkdir -p $out/bin
+            cp gta3sc $out/bin/
+            mkdir -p $out/share/gta3sc
+            cp -r config $out/share/gta3sc/
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A native script compiler/decompiler for GTA 3D Universe";
+            homepage = "https://github.com/thelink2012/gta3sc";
+            license = licenses.unfree;
+            platforms = [ "x86_64-linux" "aarch64-linux" ];
+            maintainers = [ ];
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+
+          packages = with pkgs; [
+            cmake
+            gdb
+            valgrind
+            lit
+            python3
+          ];
+
+          shellHook = ''
+            echo "gta3sc development environment loaded"
+          '';
+        };
+      }
+    );
+}
